93e64908371d5297fcc22d4d7ddc0ce00e66645b
luizguilhermejrm <luizguilhermejrm@hotmail.com>
2023-07-13 17:13:20 -0300

    feat: combobox car catalogue

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import { CustomButtonProps } from "@/types";
+
+const CustomFilter = ({ title }: CustomButtonProps) => {
+    return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,12 @@ export interface CustomButtonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CustomFilterProps {
+    title: string;
+}
