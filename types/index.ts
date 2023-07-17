a1915647c3c7a5da165c21cc7c079105c421d072
luizguilhermejrm <luizguilhermejrm@hotmail.com>
2023-07-17 16:20:01 -0300

    feat: rapid api cars

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,9 @@ export interface CustomButtonProps {
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
@@ -14,4 +17,19 @@ export interface SearchManufacturerProps {
 
 export interface CustomFilterProps {
     title: string;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
